4bd7e03effbb4c2091df8ace5c3de53cdcdea4d0
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-27 22:25:57 +0000

    minor text edit

--- test_primes.py
+++ test_primes.py
@@ -4,7 +4,7 @@ import primes
 
 def test_primes():
     testTheResults=[2,3,5,7]
-    result = primes.e(10)
+    result = primes.eratosthenes(10)
     assert testTheResults==result
     print("it works. iam here")
     
