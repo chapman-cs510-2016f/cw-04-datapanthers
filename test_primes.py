f856fe6c952f5f83ed1ef9cd655466fbc6310491
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-30 18:51:51 +0000

    added more testing

--- test_primes.py
+++ test_primes.py
@@ -6,5 +6,13 @@ def test_primes():
     testTheResults=[2,3,5,7]
     result = primes.eratosthenes(10)
     assert testTheResults==result
-    print("it works. iam here")
+    
+    testTheResults=[2,3,5]
+    result = primes.eratosthenes(7)
+    assert testTheResults==result
+    
+    
+    testTheResults=[2, 3, 5, 7, 11, 13, 17, 19]
+    result = primes.eratosthenes(20)
+    assert testTheResults==result
     
